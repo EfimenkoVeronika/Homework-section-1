5ad017305b4afd1c495d54cd6163fd8367f39db8
EfimenkoVeronika <vero.efimenko@yandex.ru>
2025-12-03 05:24:41 +1000

    Классы.

--- 1.py
+++ 1.py
@@ -3,60 +3,68 @@ import random
 class NotImplementedException(Exception):
     ...
 
-def hacking():
+class Robber:
+    def __init__(self, name, noise=None, money=None, strength=None, luck=None):
+        self.name = name
+        self.noise = noise
+        self.money = money
+        self.strength = strength
+        self.luck = luck
 
-    while True:
+    def __str__(self):
+        return f'Грабитель: имя= {self.name}: шум={self.noise}: деньги={self.money}: сила={self.strength}: удача={self.luck}'
 
-        print("Хорошо, там как раз должно быть открыто, за дверью стоит сейф!")
+    def hack_safe(self):
+        while True:
+           s = input("Какой код от сейфа (1-20)?")
+           s = int(s)
+           win = random.randint(1, 20)
 
-        s = input("Какой код от сейфа (1-20)?")
+           if s == win or random.randint(1, 20) <= self.luck:
+              print("Удача, деньги у вас в кармане!")
+              self.money += random.randint(1000, 5000)
+              return True
+           else:
+              print(f"Сейф не открылся, код {win}, сработала сигнализация!")
+              self.money += 5
+              if self.noise >= 10:
+                print("Слишком много шума! Охрана бежит!")
+                return False
 
-            #if not s.isdigit():
-            #print('Нужно было ввести число!')
-             #continue
+           repeat = input("Повторите попытку: да/нет")
+           if repeat.strip().lower() != 'да':
+            return False
 
-        s = int(s)
+def main():
+    print("Вечер добрый, напарник!")
 
-        win = random.randint(1, 20)
+    enter = input("Готов ограбить этот банк и стать самым богатым (да/нет)? ")
 
-        if s == win:
-            print("Удача, деньги у вас в кармане!")
-        else:
-            print(f"Сейф не открылся,код {win},сработала сигнализация!")
+    if enter.strip().lower() == 'да':
+        print("Давай сделаем это!")
 
-        repeat = input('Повторите попытку? (да/нет)')
-        if repeat.strip().lower() == 'нет':
-            break
+    elif enter.strip().lower() == 'нет':
+        print("Мы же так долго готовились к этому ограблению!")
 
-def window():
-    raise NotImplementedException("Разбивать окно будет громко!")
+    choose = input("Как попадем в здание банка: через запасной вход (1), через окно (2), через вентиляцию (3)")
 
-def ventilation():
-    raise NotImplementedException("Вентиляционная система не выходит к комнате с сейфом!")
+    try:
+        if choose == '1':
+           robber = Robber("Алексей", 11, 0, 5,11)
+           print(robber)
+           robber.hack_safe()
 
-print("Вечер добрый, напарник!")
+        elif choose == '2':
+            raise NotImplementedException ("Разбивать окно будет громко!")
 
-enter = input("Готов ограбить этот банк и стать самым богатым (да/нет)?")
+        elif choose == '3':
+            raise NotImplementedException("Вентиляционная система не выходит к комнате с сейфом! Сработала сигнализация!")
 
-#print('да' == enter)
+    except NotImplementedException as err:
+        print(f'Не сработало! {err}')
 
-if 'да' == enter:
-    print("Давай сделаем это!")
+    print('Бежим!')
 
-elif 'нет' == enter:
-    print("Мы же так долго готовились к этому ограблению!")
 
-choose = input("Как попадем в здание банка: через запасной вход (1), через окно (2), через вентиляцию (3)")
-
-try:
-    if '1' == choose:
-        hacking()
-    elif '2' == choose:
-        window()
-    elif '3' == choose:
-        ventilation()
-
-except NotImplementedException as err:
-    print(f'Не сработало: {err}')
-
-print('Бежим!')
+if __name__ == "__main__":
+    main()
